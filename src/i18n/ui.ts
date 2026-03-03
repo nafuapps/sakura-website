b7ac3fc7cfcd197aa7aa9df3ab1726c83ee13c06
kakashiorion <nitin.kool.1808@gmail.com>
2026-03-03 20:14:29 +0530

    Starting Hero

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -8,23 +8,50 @@ export const languages = {
 export const defaultLang = "en"
 
 export const ui = {
+  //English
   en: {
     EnrollButton: "Enroll Now",
+    Home: "Home",
     Admission: "Admission",
     Curriculum: "Curriculum",
-    About: "Admission",
-    Blogs: "Admission",
+    About: "About Us",
+    Blogs: "Blogs",
     JoinUs: "Join Us",
+    Career: "Career",
     YouTube: "YouTube",
     Instagram: "Instagram",
     Facebook: "Facebook",
-    "HeaderTicker.Location": "Baganshali, Kokrajhar, 783370",
-    "HeaderTicker.Phone": "8447695298",
+    "HeaderTicker.Location": "RNB Road, Baganshali, Kokrajhar, 783370",
+    "HeaderTicker.Phone": "9181600286",
+    "HeaderTicker.Email": "sakurapreschool.kok@gmail.com",
+    "SiteInfo.Copyright": "Copyright @ 2026 by ",
+    "SiteInfo.Developer": "Designed & developed by ",
+    "Hero.Title": "Welcome to Sakura Preschool",
+    "Hero.Subtitle": "Nurturing young minds with love and care",
+    "Hero.Primary": "Contact us",
+    "Hero.Secondary": "Explore more",
+  },
+  //Assamese
+  as: {},
+  //Bodo
+  bd: {},
+  //Hindi
+  hi: {
+    EnrollButton: "नामांकन करें",
+    Home: "होम",
+    Admission: "प्रवेश",
+    Curriculum: "पाठ्यक्रम",
+    About: "हमारे बारे में",
+    Blogs: "ब्लॉग्स",
+    JoinUs: "हमसे जुड़ें",
+    Career: "करियर",
+    YouTube: "यूट्यूब",
+    Instagram: "इंस्टाग्राम",
+    Facebook: "फेसबुक",
+    "HeaderTicker.Location": "RNB Road, Baganshali, Kokrajhar, 783370",
+    "HeaderTicker.Phone": "9181600286",
     "HeaderTicker.Email": "sakurapreschool.kok@gmail.com",
     "SiteInfo.Copyright": "Copyright @ 2026 by ",
     "SiteInfo.Developer": "Designed & developed by ",
   },
-  as: {},
-  bd: {},
-  hi: {},
 } as const
--- src/utils/commonConstants.ts
+++ src/utils/commonConstants.ts
@@ -1,6 +1,8 @@
-export const FacebookLink = ""
-export const InstagramLink = ""
-export const YouTubeLink = ""
-export const SchoolGoogleLocationLink = ""
-export const SchoolPhoneLink = "tel:8447695298"
+export const FacebookLink =
+  "https://www.facebook.com/people/Sakura-Preschool-Daycare/61586457656053/"
+export const InstagramLink = "https://www.instagram.com/sakurapreschool"
+export const YouTubeLink = "https://www.youtube.com/@sakura_preschool"
+export const SchoolGoogleLocationLink =
+  "https://maps.google.com/?q=Sakura+Preschool,+RNB+Road,+Baganshali,+Kokrajhar,+Assam+783370"
+export const SchoolPhoneLink = "tel:9181600286"
 export const SchoolEmailLink = "mailto:sakurapreschool.kok@gmail.com"
